3e646ae2ed916b1f9d65ac92e3e1d014c299bb33
donfiguerres <lyndonfiguerres@gmail.com>
2022-06-03 23:21:38 +0800

    Factored out rounding off.

--- src/Formula.test.tsx
+++ src/Formula.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import * as Formula from './Formula';
 
+test('round decimal', () => {
+    expect(Formula.roundDecimal(31.154, 2)).toEqual(31.15);
+});
+
 test('calculate bmi', () => {
-    expect(Formula.bmi(70, 1.6)).toEqual(27.34);
+  expect(Formula.bmi(70, 1.6)).toEqual(27.34);
 });
--- src/Formula.tsx
+++ src/Formula.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+export function roundDecimal(num: number, precision: number) : number {
+  return parseFloat(num.toFixed(precision));
+}
+
 export function bmi(weight: number, height: number, precision: number = 2) {
-    return parseFloat((weight / Math.pow(height, 2)).toFixed(precision));
+  return roundDecimal(weight / Math.pow(height, 2), precision);
 }
